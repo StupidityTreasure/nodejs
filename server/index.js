2538430beea04a50d9896e800d99f896b8efe039
Neha Rajpoot <neharajpoot233@gmail.com>
2024-10-18 09:31:15 +0530

    url and paths

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
 import http from "http";
-
+import fs from "fs"
+import url from 'url';
 const myserver=http.createServer((req,res)=>{
-    console.log("new req recived");
-    res.end("hii from server")
+    if(req.url==="/favicon.ico") return res.end();
+    const log=`${Date.now()}:${req.url} new req recived \n`
+    const myUrl=url.parse(req.url,true);
+    console.log(myUrl );
+    fs.appendFile("log.txt",log,(err,data)=>{
+        switch(myUrl.pathname){
+            case "/":
+                res.end("HomePage");
+                break;
+            case "/about":
+                const username=myUrl.query.myname;
+                res.end(`I am ,${username}`)
+                break;
+            default:
+                res.end("404 Not Found")
+        }
+    })
+
 });
 
 myserver.listen(8000,()=>console.log("server started"));
